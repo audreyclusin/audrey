c83768d4bc07a5b1ce62400317b53d49af24e917
Audrey Clusin <audrey_clusin@berkeley.edu>
2025-09-08 16:39:28 -0700

    Initial commit

---
+++ homework1/homework1.py
@@ -0,0 +1,77 @@
+# File: homework1. py
+
+# -- Variables and Data Types --
+
+a = 10
+print(a)
+print(type(a)) 
+# a is an interger
+
+b = 1.5
+print(b)
+print(type(b)) 
+# b is a float 
+
+c = 3j
+print(c)
+print(type(c))
+# c is complex  
+
+d = "hello"
+print(d)
+print(type(d)) 
+# d is a string 
+
+e = [1, 2, 3]
+print(e)
+print(type(e)) 
+# e is a list
+
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+print(f)
+print(type(f)) 
+# f is a dictionary
+
+g = (1, 2)
+print(g)
+print(type(g)) 
+# g is a tuple
+
+h = ["apple", "banana", "strawberry"]
+print(h)
+print(type(h)) 
+# h is a list
+
+i = True
+print(i)
+print(type(i)) 
+# i is a boolean
+
+j = None
+print(j)
+print(type(j)) 
+# j is NoneType
+
+k = [True, "blue", 12]
+print(k)
+print(type(k)) 
+# k is a list
+
+l = str(14)
+print(l)
+print(type(l)) 
+# l is a string
+
+m = 1e4
+print(m)
+print(type(m)) 
+# m is a float
+
+"""
+1. I found 9 types of data
+2. String, list, boolean, float, tuple, dictionary, complex, NoneType, integer 
+3. (b and m), (l and d), (k, h, and e)
+4. String - the function str () acted on the integer 14 and defined it as a string instead. 
+5. 
+
+"""
